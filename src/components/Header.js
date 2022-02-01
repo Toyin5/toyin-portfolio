d7b89acc5978515df7c5b5ef4f5ae9a15a816c5f
toyin5 <toyinmuhammed50@gmail.com>
2022-02-01 19:10:31 +0200

    Added more stylings to the header + icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./Header.css"
+import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle, AiOutlineWhatsApp } from 'react-icons/ai';
+import {FaLinkedin, FaLinkedinIn, FaTelegram} from "react-icons/fa"
+import { DiCssdeck } from 'react-icons/di';
 
 function Header() {
   return (
   <div className="header">
    	<div className="full-name">
-		<span>Toyin Muhammed</span>
+		<DiCssdeck /> <span>Toyin Muhammed</span>
 	</div>
 	<div className="nav">
 			<h4>Home</h4>
@@ -14,7 +17,11 @@ function Header() {
 			<h4>Contact</h4>
 	</div>
 	<div className="social-links">
-		<a href="https://github.com/Toyin5">{/*Github Icon*/}</a>
+		<a href="https://github.com/Toyin5"><AiFillGithub fill="#303030" /></a>
+		<a href="https://github.com/Toyin5"><AiOutlineWhatsApp fill="#303030" /></a>
+		<a href="https://github.com/Toyin5"><AiFillTwitterCircle fill="#303030" /></a>
+		<a href="https://github.com/Toyin5"><FaLinkedinIn fill="#303030" /></a>
+		<a href="https://github.com/Toyin5"><FaTelegram fill="#303030" /></a>
 	</div>
   </div>
   );
